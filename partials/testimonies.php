95a36fd04db225df9a3c07d0773ce60049efca49
Josh Dabuet <josiah@lightmedia.com.au>
2020-06-04 22:46:15 +0800

    Updated voice/testimonies text

--- partials/testimonies.php
+++ partials/testimonies.php
@@ -11,12 +11,12 @@
 					<img src="public/images/testimony-1.jpg" alt="">
 				</div>
 				<div class="testimony__bottom">
-					<h3 class="testimony__title">CV数が157%増加、受注数も147%増加しました！</h3>
+					<h3 class="testimony__title">CV数が157%増加、受注数も147%増加しました</h3>
 					<dl class="testimony__details">
 						<dt>導入前の課題</dt>
-						<dd>インバウンドによるお問い合わせ率、受注の獲得率の伸び悩みが課題を感じていました。</dd>
+						<dd>インバウンドによるお問い合わせ率、受注の獲得率の伸び悩みが課題と感じていた。</dd>
 						<dt>導入効果</dt>
-						<dd>導入前と比べ、お問い合わせ数は157%増加しました。<br class="u-show-pc">またお問い合わせ増加により、受注率も147%増加につながりました。<br>チャット導入により、有人での対応が不要な問い合わせの対応が不要になり、より受注につなげるための時間を確保できたことがよかったです。</dd>
+						<dd>導入前と比べ、お問い合わせ数は157%増加しました。<br class="u-show-pc"> また、お問い合わせ増加により、受注率も147%増加につながりました。チャットボット導入により、有人での対応が不要な問合せの対応が不要になり、より受注につなげるための時間を確保できたことが良かった。</dd>
 					</dl>
 				</div>
 			</div>
@@ -25,15 +25,15 @@
 			<div class="testimony">
 				<div class="testimony__top">
 					<span class="testimony__from">不動産業</span>
-					<img src="public/images/testimony-2.jpg" alt="">
+					<img src="public/images/testimony-1.jpg" alt="">
 				</div>
 				<div class="testimony__bottom">
-					<h3 class="testimony__title">CV数が157%増加、受注数も147%増加しました！</h3>
+					<h3 class="testimony__title">チャット経由で400件を案件化！売上につながった</h3>
 					<dl class="testimony__details">
 						<dt>導入前の課題</dt>
-						<dd>インバウンドによるお問い合わせ率、受注の獲得率の伸び悩みが課題を感じていました。</dd>
+						<dd>WEBサイトのコンテンツ改善だけでは、サイトからの離脱率改善に限界を感じていた。</dd>
 						<dt>導入効果</dt>
-						<dd>導入前と比べ、お問い合わせ数は157%増加しました。<br class="u-show-pc">またお問い合わせ増加により、受注率も147%増加につながりました。<br>チャット導入により、有人での対応が不要な問い合わせの対応が不要になり、より受注につなげるための時間を確保できたことがよかったです。</dd>
+						<dd>チャットによる対話型のWEB接客を実現したことにより、離脱率の改善につながりました。チャットボットの導入により、半年の間でチャット経由で400件を案件に成功し、売上につなげることができました。</dd>
 					</dl>
 				</div>
 			</div>
@@ -41,16 +41,67 @@
 		<li class="l-testimonies__item">
 			<div class="testimony">
 				<div class="testimony__top">
-					<span class="testimony__from">インターネット・IT3</span>
-					<img src="public/images/testimony-3.jpg" alt="">
+					<span class="testimony__from">インターネット・IT</span>
+					<img src="public/images/testimony-1.jpg" alt="">
 				</div>
 				<div class="testimony__bottom">
-					<h3 class="testimony__title">CV数が157%増加、受注数も147%増加しました！</h3>
+					<h3 class="testimony__title">約1年で1,100件以上の問合せ受注額も1億円以上に</h3>
 					<dl class="testimony__details">
 						<dt>導入前の課題</dt>
-						<dd>インバウンドによるお問い合わせ率、受注の獲得率の伸び悩みが課題を感じていました。</dd>
+						<dd>CV数の伸び悩みが課題と感じていた。お問い合わせフォーム以外からの、インバウンドの新しい施策を模索していました。</dd>
 						<dt>導入効果</dt>
-						<dd>導入前と比べ、お問い合わせ数は157%増加しました。<br class="u-show-pc">またお問い合わせ増加により、受注率も147%増加につながりました。<br>チャット導入により、有人での対応が不要な問い合わせの対応が不要になり、より受注につなげるための時間を確保できたことがよかったです。</dd>
+						<dd>チャットボット導入により、約1年間で1,100件以上もチャット経由のお問い合わせにつながりました。資料送付やアポイントにもつながりました。結果として、売上にもつながり、受注額も1億円以上になっています。</dd>
+					</dl>
+				</div>
+			</div>
+		</li>
+		<li class="l-testimonies__item">
+			<div class="testimony">
+				<div class="testimony__top">
+					<span class="testimony__from">インターネット・IT</span>
+					<img src="public/images/testimony-1.jpg" alt="">
+				</div>
+				<div class="testimony__bottom">
+					<h3 class="testimony__title">導入後の顧客満足度80%以上を継続している</h3>
+					<dl class="testimony__details">
+						<dt>導入前の課題</dt>
+						<dd>従来のサポート窓口では、膨大な件数のお問い合わせを限られた人員で対応していたため、質の高いサポートを実現しきれず、顧客満足度も高いとは言えなかった。</dd>
+						<dt>導入効果</dt>
+						<dd>チャットボットによる自動回答で、質の高いサポートを必要するお問い合わせを選別することができ、限られた人員でも丁寧な対応を実現できた。その結果、導入後5ヶ月以上連続で顧客満足度80%以上を継続できています。</dd>
+					</dl>
+				</div>
+			</div>
+		</li>
+		<li class="l-testimonies__item">
+			<div class="testimony">
+				<div class="testimony__top">
+					<span class="testimony__from">宅配水</span>
+					<img src="public/images/testimony-1.jpg" alt="">
+				</div>
+				<div class="testimony__bottom">
+					<h3 class="testimony__title">業務の80%以上をチャットに移行し業務効率化に成功</h3>
+					<dl class="testimony__details">
+						<dt>導入前の課題</dt>
+						<dd>電話でのお問い合わせ対応ではサポートコストが増大し、コールセンターでのきめ細やかな対応が困難になっていました。</dd>
+						<dt>導入効果</dt>
+						<dd>コールセンターの業務範囲のうち、80%以上はチャットに移行でき、さらにチャット問い合わせの85%をチャットボットで自動完結させることができました。その結果、業務を効率化することに成功し、きめ細やかな対応を行うことができるようになりました。</dd>
+					</dl>
+				</div>
+			</div>
+		</li>
+		<li class="l-testimonies__item">
+			<div class="testimony">
+				<div class="testimony__top">
+					<span class="testimony__from">人材派遣</span>
+					<img src="public/images/testimony-1.jpg" alt="">
+				</div>
+				<div class="testimony__bottom">
+					<h3 class="testimony__title">チャットボットと有人対応を併用し、売上アップ</h3>
+					<dl class="testimony__details">
+						<dt>導入前の課題</dt>
+						<dd>"派遣スタッフから、仕事内容や持参物に関する電話問い合わせが相当数発生<br>電話がつながりにくいことが多く、スタッフへのサポート品質の低下となっていた。"</dd>
+						<dt>導入効果</dt>
+						<dd>よくある質問はチャットボットで自動回答、高度な質問は有人対応するように自動回答と有人対応を切り分け、併用し運用しました。その結果、派遣スタッフからの苦情が減り、売上アップにもつながりました。</dd>
 					</dl>
 				</div>
 			</div>
